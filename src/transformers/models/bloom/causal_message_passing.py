5b1cadfa0bc3b44a3fb6265c3c5491080a7ffb75
root <zacharesp@gmail.com>
2023-05-07 15:15:15 +0000

    flamingo inspired

--- src/transformers/models/bloom/causal_message_passing.py
+++ src/transformers/models/bloom/causal_message_passing.py
@@ -1,4 +1,5 @@
 """ A set of functions to perform message passing on a serialized graph in an LLM """
+
 import enum
 from collections import defaultdict
 import itertools
@@ -6,6 +7,7 @@ from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 import torch
+from torch_scatter import scatter, scatter_softmax
 import torch_geometric
 
 from .desequence_graph_ids import SequenceElement
@@ -26,9 +28,23 @@ def build_message_passing_matrices(
     """
     message_passing_dicts = []
     for edge_sequence in edge_sequences:
-        message_passing_dict = {'tokens2edges': [], 'edges2tokens': [], 'inverse_edge_index': []}
+        message_passing_dict = {'tokens2edges': [], 'edges2tokens': [], 'edge_index': []}
         node2edge_idxs = defaultdict(list)
-        assert len(edge_sequence) > 0
+        pred_node, edge, succ_node = edge_sequence[-1]
+        if isinstance(succ_node, SequenceElement):
+            sequence_end_idx = succ_node.end_idx
+        elif isinstance(edge, SequenceElement):
+            sequence_end_idx = edge.end_idx
+        else:
+            sequence_end_idx = pred_node.end_idx
+        pred_node, edge, succ_node = edge_sequence[0]
+        if isinstance(succ_node, SequenceElement):
+            sequence_start_idx = succ_node.end_idx
+        elif isinstance(edge, SequenceElement):
+            sequence_start_idx = edge.end_idx
+        else:
+            sequence_start_idx = pred_node.end_idx
+        message_passing_dict['slice_idxs'] = (sequence_start_idx, sequence_end_idx)
         for edge_idx, sequenced_edge in enumerate(edge_sequence):
             pred_node, edge, succ_node = sequenced_edge
             node2edge_idxs[pred_node.ids].append(edge_idx)
@@ -40,13 +56,8 @@ def build_message_passing_matrices(
             else:
                 end_idx = pred_node.end_idx
             message_passing_dict['tokens2edges'].append(end_idx - 1)
-            message_passing_dict['edges2tokens'].append(pred_node.start_idx)
-        message_passing_dict['tokens2edges'] = torch.from_numpy(
-            np.array(message_passing_dict['tokens2edges'])
-        ).long().to(token_ids.device)
-        message_passing_dict['edges2tokens'] = torch.from_numpy(
-            np.array(message_passing_dict['edges2tokens'])
-        ).long().to(token_ids.device)
+            for sequence_idx in range(end_idx, sequence_end_idx):
+                message_passing_dict['edges2tokens'].append([edge_idx, sequence_idx])
         message_passing_dict['edge_index'] = []
         for edge_idxs in node2edge_idxs.values():
             if len(edge_idxs) < 2:
@@ -55,6 +66,17 @@ def build_message_passing_matrices(
                 message_passing_dict['edge_index'].append(
                     [idx0, idx1] if idx0 < idx1 else [idx1, idx0]
                 )
+        message_passing_dict['tokens2edges'] = torch.from_numpy(
+            np.array(message_passing_dict['tokens2edges'])
+        ).long().to(token_ids.device)
+        if len(message_passing_dict['edges2tokens']) > 0:
+            message_passing_dict['edges2tokens'] = torch.from_numpy(
+                np.array(message_passing_dict['edges2tokens']).transpose(1, 0)
+            ).long().to(token_ids.device)
+        else:
+            message_passing_dict['edges2tokens'] = torch.from_numpy(
+                np.array(message_passing_dict['edges2tokens'])
+            ).long().to(token_ids.device)
         if len(message_passing_dict['edge_index']) > 0:
             message_passing_dict['edge_index'] = torch.from_numpy(
                 np.array(message_passing_dict['edge_index']).transpose(1, 0)
@@ -67,10 +89,37 @@ def build_message_passing_matrices(
     return message_passing_dicts
 
 
+def graph_cross_attention(
+    values: torch.Tensor,
+    key_representations: torch.Tensor,
+    query_representations: torch.Tensor,
+    edge_index: torch.Tensor
+) -> torch.Tensor:
+    """ Performs graph attention on a set of prior probabilities using the representation of each
+        node in the graph to calculate the attention weights. The implemented attention is dot
+        product attention as implemented in the transformer architecture
+    """
+    scaling_constant = torch.Tensor(
+        np.sqrt([key_representations.size(1)])
+    ).to(key_representations.device)
+    dot_products = (
+        query_representations[edge_index[0]]
+        * key_representations[edge_index[1]]
+    ).sum(1) / scaling_constant
+    weights = scatter_softmax(src=dot_products, index=edge_index[1], dim=0)
+    weighted_probs = weights.unsqueeze(1) * values[edge_index[0]]
+    return scatter(src=weighted_probs, index=edge_index[1], dim=0)
+
+
 class CausalMessagePassingLayer(torch.nn.Module):
     def __init__(self, gnn_type: str, embedding_size: int):
         super().__init__()
         self.gnn_layer = GNNLayerFactory[gnn_type].value(embedding_size, embedding_size)
+        self.gating_parameter_a = torch.nn.Parameter(torch.zeros(1))
+        self.gating_parameter_b = torch.nn.Parameter(torch.zeros(1))
+        self.key_embedder = torch.Linear(embedding_size, 64)
+        self.query_embedder = torch.Linear(embedding_size, 64)
+        self.linear_layer = torch.nn.Linear(embedding_size, embedding_size)
 
     def forward(
         self,
@@ -81,19 +130,21 @@ class CausalMessagePassingLayer(torch.nn.Module):
         for t_embeddings, message_passing_dict in zip(token_embeddings, message_passing_dicts):
             edge_embeddings = t_embeddings[message_passing_dict['tokens2edges']]
             if message_passing_dict['edge_index'].numel() > 0:
-                edge_embeddings = self.gnn_layer(
-                    edge_embeddings,
-                    message_passing_dict['edge_index']
+                edge_embeddings = self.gnn_layer(edge_embeddings, message_passing_dict['edge_index'])
+            graph_attention_embeddings = torch.zeros_like(t_embeddings)
+            if message_passing_dict['edges2tokens'].numel() > 0:
+                start_idx, end_idx = message_passing_dict['slice_idxs']
+                graph_attention_embeddings[start_idx, end_idx] = graph_cross_attention(
+                    values=edge_embeddings,
+                    key_representations=self.key_embedder(edge_embeddings),
+                    query_representations=self.query_embedder(t_embeddings),
+                    edge_index=message_passing_dict['edges2tokens']
                 )
-            if edge_embeddings.shape[0] > 1:
-                causal_edge_embeddings = torch.cat([
-                        torch.zeros_like(edge_embeddings[0]).unsqueeze(0),
-                        edge_embeddings[:-1],
-                ], dim=0)
-            else:
-                causal_edge_embeddings = torch.zeros_like(edge_embeddings)
-            new_t_embeddings = torch.zeros_like(t_embeddings)
-            new_t_embeddings[message_passing_dict['edges2tokens']] = causal_edge_embeddings
+            new_t_embeddings = t_embeddings + (torch.tanh(self.gating_parameter_a) * graph_attention_embeddings)
+            new_t_embeddings = (
+                new_t_embeddings
+                + (torch.tanh(self.gating_parameter_b) * self.linear_layer(new_t_embeddings))
+            )
             new_token_embeddings.append(new_t_embeddings.unsqueeze(0))
         return token_embeddings + torch.cat(new_token_embeddings, dim=0)
 
